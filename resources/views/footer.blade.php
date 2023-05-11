b77a300745b37e37cfb0b5377f2a815aa11161ef
fredrickmugish <fredrickgotfried@gmail.com>
2023-05-11 09:29:22 -0400

    initial commit

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,53 @@
+<footer id="footer" class="footer">
+    <div class="footer-top">
+      <div class="container">
+        <div class="row gy-4">
+          <div class="col-lg-5 col-md-12 footer-info">
+            <a href="index.html" class="logo d-flex align-items-center">
+              <span>MTS TECH</span>
+            </a>
+            <strong><p>Follow Us on social Media to connect with us</p></strong>
+            <div class="social-links mt-3">
+              <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+              <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+              <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+              <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
+            </div>
+          </div>
+
+          <div class="col-lg-2 col-6 footer-links">
+            <h4>Mission</h4>
+        <strong><p>To make your business better through technology</p></strong> 
+          </div>
+
+          <div class="col-lg-2 col-6 footer-links">
+            <h4>Our Services</h4>
+            <ul>
+              <strong><li><i class="bi bi-chevron-right"></i> Web Design</li></strong>
+              <strong><li><i class="bi bi-chevron-right"></i> Web Development </li></strong>
+             <strong><li><i class="bi bi-chevron-right"></i> Software Development</li></strong> 
+              <strong><li><i class="bi bi-chevron-right"></i>Graphic Design</li></strong>
+            </ul>
+          </div>
+
+          <div class="col-lg-3 col-md-12 footer-contact text-center text-md-start">
+            <h4>Contact Us</h4>
+            <p>
+             
+              <strong>Phone:</strong> +1 5589 55488 55<br>
+              <strong>Email:</strong> info@example.com<br>
+            </p>
+
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong><span>Mts tech solutions</span></strong>. All Rights Reserved
+      </div>
+      
+    </div>
+  </footer>
